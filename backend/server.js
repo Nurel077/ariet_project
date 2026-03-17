15661b6036f5a27dc8283d52a53848a38f327624
x6m <freezlite150@gmail.com>
2026-03-17 10:15:36 +0600

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,43 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import "dotenv/config";
 import { connectDB } from "./config/db.js";
+
 import foodRouter from "./routes/foodRoute.js";
 import userRouter from "./routes/userRoute.js";
-import "dotenv/config";
 import cartRouter from "./routes/cartRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
+// config env
+dotenv.config();
+
 // app config
 const app = express();
-const port =process.env.PORT || 4000;
+const port = process.env.PORT || 4000;
 
-//middlewares
+// middlewares
 app.use(express.json());
 app.use(cors());
 
-// DB connection
-connectDB();
+// database connection
+connectDB(process.env.MONGO_URI);
 
-// api endpoints
+// API routes
 app.use("/api/food", foodRouter);
-app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
+// static images
+app.use("/images", express.static("uploads"));
+
+// test route
 app.get("/", (req, res) => {
   res.send("API Working");
 });
 
+// server start
 app.listen(port, () => {
-  console.log(`Server Started on port: ${port}`);
+  console.log(`🚀 Server Started on port: ${port}`);
 });
